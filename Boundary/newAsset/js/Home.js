9d9fcf3b7cb221e1c7fe1eae9311e6cd1859d50a
mjhmjh385 <mjh385@gmail.com>
2017-03-23 00:12:42 +0430

    added new ui to project by mjh

---
+++ Boundary/newAsset/js/Home.js
@@ -0,0 +1,30 @@
+﻿angular.module('Home', [])
+
+ .controller('HomeCtrl', ['$scope', function ($scope) {
+
+
+     $scope.showRegisterBox = function () {
+         $('.popUpMadule').fadeIn();
+         $('.signUpBox').slideDown();
+     }
+
+     $scope.showLoginBox = function () {
+         $('.popUpMadule').fadeIn();
+         $('.loginBox').slideDown();
+     }
+
+     $scope.closeBox = function () {
+         $('.popUpMadule').fadeOut();
+         $('.loginBox').slideUp();
+         $('.signUpBox').slideUp();
+
+     }
+
+
+
+
+ }]);
+
+
+
+
