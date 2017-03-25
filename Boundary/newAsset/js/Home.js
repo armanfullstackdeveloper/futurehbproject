007b0dfafac579666770b00d7bb5466d0e28eedd
mjhmjh385 <mjh385@gmail.com>
2017-03-26 00:26:12 +0430

    added crusel

--- Boundary/newAsset/js/Home.js
+++ Boundary/newAsset/js/Home.js
@@ -20,7 +20,33 @@
 
      }
 
+     owl1= $('.owl-carousel').owlCarousel({
+         rtl: true,
+         loop: false,
+         margin: 10,
+         center:true,
+         nav: false,
+          responsive: {
+             0: {
+                 items: 1
+             },
+             600: {
+                 items: 3
+             },
+             1000: {
+                 items: 5
+             }
+         }
+     })
+      
 
+     $(".prev1").click(function () {
+         owl1.trigger('prev.owl.carousel');
+     });
+
+     $(".next1").click(function () {
+         owl1.trigger('next.owl.carousel');
+     });
 
 
  }]);
